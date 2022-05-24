2d1763ae9b50b34fa04d81b40aeec5eaade3cfc7
spiros <s.denaxas@gmail.com>
2022-05-24 15:06:53 +0200

    MFA done

--- raw/kuan_multimorbid_freqapp/server.R
+++ raw/kuan_multimorbid_freqapp/server.R
@@ -7,6 +7,15 @@ library(DT)
 
 load("tables_top50_triads.rda")
 
+round2 = function(x, n) {
+  posneg = sign(x)
+  z = abs(x)*10^n
+  z = z + 0.5 + sqrt(.Machine$double.eps)
+  z = trunc(z)
+  z = z/10^n
+  z*posneg
+}
+
 function(input, output) {
 
 table_top50_triad<-reactive({ 
@@ -30,7 +39,7 @@ else if  (input$dataset == "Age 80+") {age_80_top50_triads}
 
  observe({
 output$dataTable <- DT::renderDT({
-    datatable(table_top50_triad() %>% select(c(`Disease Triad` = Triad ,`Prevalence (%)**`= Prevalence, `Low Conf Int` = LCI, `Upp Conf Int` = UCI )),rownames=F) 
+    datatable(table_top50_triad() %>% mutate(Prevalence=format(round2(Prevalence, 4),nsmall=4)) %>% mutate(LCI=format(round2(LCI, 4),nsmall=4)) %>% mutate(UCI=format(round2(UCI, 4),nsmall=4)) %>% select(c(`Disease Triad` = Triad ,`Prevalence (%)`= Prevalence, `Low Conf Int` = LCI, `Upp Conf Int` = UCI )),rownames=F) 
 })
 })
 
--- raw/kuan_multimorbid_freqapp/ui.R
+++ raw/kuan_multimorbid_freqapp/ui.R
@@ -7,17 +7,26 @@ library(DT)
 
 load("tables_top50_triads.rda")
 
+sidebarPanel2 <- function (..., out = NULL, width = 4) 
+{
+  div(class = paste0("col-sm-", width), 
+    tags$form(class = "well", ...),
+    out
+  )
+}
+
 fluidPage(
 
 #  titlePanel(h1("Multimorbidity Frequency App", h3("50 most prevalent disease triads"))),
    
    titlePanel(h1("Multimorbidity Frequency App")),
 
-  sidebarPanel(
+  sidebarPanel2(
 
 
     selectInput('dataset', 'Dataset', c("Whole", "Asian", "Black", "White", "Female", "Male", "Age 0-9+", "Age 10-19+", "Age 20-29+", "Age 30-39+", "Age 40-49+", "Age 50-59+", "Age 60-69+", "Age 70-79+", "Age 80+"), selected="Whole"), 
 
+	out = h5("Infections were grouped by causal organism and organ system according to the dual-axis ICD-10 classification system and could therefore be more highly represented in disease triads. This should be taken into account when interpreting the disease triads.")
   ),
 
 
