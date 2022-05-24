b0e2d99620ed3b11e2c82f3695a91adf230a47b7
spiros <s.denaxas@gmail.com>
2022-05-24 15:30:54 +0200

    updated shiny app

--- raw/kuan_comorbid_byfreq/ui.R
+++ raw/kuan_comorbid_byfreq/ui.R
@@ -23,22 +23,16 @@ sidebarPanel2 <- function (..., out = NULL, width = 4)
 
 fluidPage(
 
-  titlePanel("Most Common Comorbidities"),
+  titlePanel(h1("Comorbidity Frequency App")),
 
 
   sidebarPanel2(
 
-#    selectInput('ethnicity', 'Ethnicity', c("All Ethnicities", "Asian", "Black", "White"), selected="All Ethnicities"), 
-
-
-#    selectInput('sex', 'Sex', c("Both sexes", "Female", "Male"), selected="Both sexes"), 
-
-#    selectInput('age', 'Age', c("All ages", "Age 0-9+", "Age 10-19+", "Age 20-29+", "Age 30-39+", "Age 40-49+", "Age 50-59+", "Age 60-69+", "Age 70-79+", "Age 80+"), selected="All ages"), 
 
 
     selectInput('dz', 'Index Disease', sort(labels$Disease), selected="Macular Degeneration"), 
 
-    selectInput('dataset', 'Dataset', c("Whole", "Asian", "Black", "White", "Female", "Male", "Age 0-9+", "Age 10-19+", "Age 20-29+", "Age 30-39+", "Age 40-49+", "Age 50-59+", "Age 60-69+", "Age 70-79+", "Age 80+"), selected="Whole"), 
+    selectInput('dataset', 'Dataset', c("Whole", "Asian", "Black", "White", "Female", "Male", "Age 0-9+", "Age 10-19+", "Age 20-29+", "Age 30-39+", "Age 40-49+", "Age 50-59+", "Age 60-69+", "Age 70-79+", "Age 80+"), selected="Whole"),
 
 #	out = h4("Prevalence (%) of index disease with the most common comorbidities"),
 
@@ -49,7 +43,7 @@ fluidPage(
 
 
   mainPanel(
-#   plotOutput('plot')
+	h2("Most common comorbidities"),
      tabsetPanel(
         tabPanel("Plot", plotOutput("plot")), 
         tabPanel("Table", dataTableOutput("dataTable"), htmlOutput("txt1"), htmlOutput("txt2"))
